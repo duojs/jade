355ded4acdfd522ebed1ac43df6b902a6d3c63b1
Matthew Mueller <mattmuelle@gmail.com>
2014-03-31 00:52:39 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * Module Dependencies
+ */
+
+var fs = require('co-fs');
+var jade = require('jade');
+var filepath = require.resolve('jade/runtime.js');
+
+/**
+ * Export `transform`
+ */
+
+module.exports = transform;
+
+/**
+ * Initialize `transform`
+ *
+ * @param {Object} opts
+ * @return {*function} fn
+ * @api public
+ */
+
+function transform(opts) {
+  opts = opts || {};
+  var first = true;
+
+  return function *(src, json, builder) {
+    if (first) {
+      var runtime = yield fs.readFile(filepath, 'utf8');
+      builder.include('jade-runtime', runtime);
+    }
+
+    return 'var jade = require(\'jade-runtime\');\n\n' +
+           'module.exports = ' + jade.compileClient(src) + ';';
+
+    first = false;
+  }
+}
